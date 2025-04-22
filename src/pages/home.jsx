8ecd3d3feaa3f8a219384c302b4d05710f957e7f
Abdellah <smahiabdellahv&@gmail.com>
2025-04-22 14:13:52 +0100

    update home section

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -12,19 +12,19 @@ function Home() {
         </div>
       <section
         id="home"
-        className="w-full min-h-screen flex justify-center items-center max-md:flex-col-reverse pt-25"
+        className="w-full min-h-screen flex max-md:flex-col-reverse py-20 md:py-50 px-5 md:px-15  gap-15"
       >
-        <div className="w-[70%] max-md:w-[100%] flex flex-col items-center gap-8 z-10">
-          <h1 className="w-fit h-fit text-center text-5xl max-md:text-4xl tracking-wide font-bold p-2 bg-gradient-to-r from-slate-500 to-violet-600 text-transparent bg-clip-text cursor-default">
+        <div className="flex flex-col gap-5 z-10">
+          <h1 className="flex self-center w-fit h-fit text-center text-5xl max-md:text-4xl tracking-wide font-bold p-2 bg-gradient-to-r from-slate-500 to-violet-600 text-transparent bg-clip-text cursor-default">
             Hey , I'm Abdellah Smahi
           </h1>
-          <p className="w-[65%] max-md:w-[80%] text-center text-[16px] text-gray-400 leading-10 cursor-default">
+          <p className="text-center text-[16px] text-gray-400 leading-10 cursor-default">
             I'm a frontend developer with a fair expertise in the React js
             framework . I build scalable and interactive single-page
             applications, leveraging modern JavaScript and best practices . and
             efficient rendering strategies to deliver good user experiences.
           </p>
-          <div className="flex justify-around w-[50%] max-md:w-[80%]">
+          <div className="flex justify-around self-center md:w-[70%]">
             <button className="max-sm:text-[12px] px-5 py-3 border-indigo-700 border-2 bg-gradient-to-r from-indigo-700 to-violet-700 hover:opacity-70 hover:-translate-y-1 rounded-md duration-300 cursor-pointer">
               <a href="#projects">View Project</a>
             </button>
@@ -34,10 +34,10 @@ function Home() {
             </button>
           </div>
         </div>
-        <div className="w-[18%]">
+        <div>
           <img
             src={profile}
-            className="border-1 border-gray-800 hover:shadow-indigo-900 hover:shadow-2xl duration-300 hover:-translate-y-2 opacity-75 rounded-full cursor-pointer"
+            className="max-md:w-[60%] mx-auto border-1 border-gray-800 hover:shadow-indigo-900 hover:shadow-2xl duration-300 hover:-translate-y-2 opacity-75 rounded-full cursor-pointer"
             alt=""
           />
         </div>
